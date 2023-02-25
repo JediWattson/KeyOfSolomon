290e1844d003631e84cb65dbd5ce1df313a46c2e
Robert Daly <JediWattson@users.noreply.github.com>
2023-02-24 23:44:29 -0500

    enhanced personality of ai

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -11,17 +11,29 @@ export default async function handler(req, res) {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `
-        Oracle is a chatbot that reluctantly answers questions with sarcastic responses:
+        Oracle can fortell fictional futures and describes visions vividly with great detail:
         ${text}
       `,
+      stream: true,
       temperature: 0.5,
-      max_tokens: 60,
+      max_tokens: 230,
       top_p: 0.3,
       frequency_penalty: 0.5,
       presence_penalty: 0.0,
-    });
+    }, { responseType: 'stream' });
 
-    res.status(200).json(response.data);
+    let oracleRes = ''
+    response.data.on('data', data => {
+      const lines = data.toString().split('\n').filter(line => line.trim() !== '');
+      lines.forEach(line => {        
+        const message = line.replace(/^data: /, '');
+        if (message === '[DONE]') {
+          return res.status(200).json({ text: oracleRes })
+        } else {
+          oracleRes += JSON.parse(message).choices[0].text;
+        }
+      });
+    })
   } catch (error) {
     console.error(error);
     res.status(500);
--- src/chat/index.tsx
+++ src/chat/index.tsx
@@ -5,19 +5,18 @@ import Textarea from '../textarea'
 import styles from './style.module.css';
 
 const Chat = () => {
-    const [oracleSays, setOracle] = useState(["Oracle: Ask me a question, go ahead, I'm waiting!"])
+    const [oracleSays, setOracle] = useState("Oracle: Ask me a question, go ahead, I'm waiting!")
     const textValueRef = useRef("");
     const handleClick = async () => {
-        setOracle(["Oracle: Let me think about that..."])
+        setOracle("Oracle: Let me think about that...")
         const res = await fetch('/api/chat', { method: "POST", body: JSON.stringify({ text: textValueRef.current }) });
         const data = await res.json();
-        const oracleText = data.choices.map(c => c.text);
-        setOracle(oracleText);
+        setOracle(data.text);
     }
     return (
         <>
             <div className={styles.textBox}>
-                {oracleSays.map((text, i) => <p className={styles.oracleText} key={i}>{text}</p>)}
+                {oracleSays}
             </div>
             <div className={styles.actions}>
                 <Button onClick={handleClick} text="Send" />

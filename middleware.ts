81f35799c439e089b6c453b96d2f5a0d3ab98ccd
Robert Daly <JediWattson@users.noreply.github.com>
2024-01-10 15:26:11 -0500

    cleaned up reqs in middleware to prevent reassigning tokens, fixed up string to use date

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,19 @@ import { v4 as uuidv4 } from "uuid";
 
 const sessionKey = "session-id";
 export async function middleware(request: NextRequest) {
+  // this could be a bug later on :(
+  if (request.headers.get("purpose") === 'prefetch') return NextResponse.next();
+  
+  const date = new Date();
+  const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}:{${date.getHours()}:${date.getMinutes()}}`;
+
   if (request.cookies.has(sessionKey)) {
     console.log(
-      `USER RETURN: ${request.cookies.get(sessionKey)?.value} -- ${request.url}`
+      `USER RETURN: ${formattedDate} -- ${request.cookies.get(sessionKey)?.value} -- ${request.url}`
     );
   } else {
     const sessionValue = uuidv4();
-    console.log(`USER VISIT: ${sessionValue}`);
+    console.log(`USER VISIT: ${formattedDate} -- ${sessionValue}`);
     const response = NextResponse.next();
     response.cookies.set({
       name: sessionKey,
@@ -24,5 +30,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/((?!api|_next|favicon.ico).*)",
+  matcher: "/((?!api|_next|.*.ico|.*.jpeg|.*.jpg|.*.png|.*.css|.*.js).*)"
 };

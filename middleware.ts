2bc1cd5a5087d39873f41ab52f525d033dcd8156
Robert Daly <JediWattson@users.noreply.github.com>
2024-01-10 15:34:03 -0500

    remove date, use count to tract user numbers

--- middleware.ts
+++ middleware.ts
@@ -3,21 +3,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
+let instCount = 0
 const sessionKey = "session-id";
 export async function middleware(request: NextRequest) {
   // this could be a bug later on :(
   if (request.headers.get("purpose") === 'prefetch') return NextResponse.next();
-  
-  const date = new Date();
-  const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}:{${date.getHours()}:${date.getMinutes()}}`;
 
   if (request.cookies.has(sessionKey)) {
     console.log(
-      `USER RETURN: ${formattedDate} -- ${request.cookies.get(sessionKey)?.value} -- ${request.url}`
+      `USER RETURN: ${request.cookies.get(sessionKey)?.value} -- ${request.url}`
     );
   } else {
+    instCount++
     const sessionValue = uuidv4();
-    console.log(`USER VISIT: ${formattedDate} -- ${sessionValue}`);
+    console.log(`USER VISIT: #${instCount} -- ${sessionValue}`);
     const response = NextResponse.next();
     response.cookies.set({
       name: sessionKey,
